688ae02b302258e512148966e2f5ddf208e09de3
chenzhuoyu <chenzhuoyu@bytedance.com>
2022-05-12 16:18:50 +0800

    fix: DFS the dominator tree for CSE pass

--- internal/atm/ssa/pass_comsubexpr.go
+++ internal/atm/ssa/pass_comsubexpr.go
@@ -20,7 +20,7 @@ import (
     `fmt`
 )
 
-type _VID interface {
+type _Vid interface {
     IrDefinations
     vid() []string
 }
@@ -83,68 +83,93 @@ func (self *IrBitTestSet) vid() []string {
     }
 }
 
+type _VidMap struct {
+    p *_VidMap
+    m map[string]Reg
+}
+
+func (self *_VidMap) derive() *_VidMap {
+    return &_VidMap {
+        p: self,
+        m: make(map[string]Reg),
+    }
+}
+
+func (self *_VidMap) lookup(vid string) (Reg, bool) {
+    if r, ok := self.m[vid]; ok {
+        return r, true
+    } else if self.p == nil {
+        return 0, false
+    } else {
+        return self.p.lookup(vid)
+    }
+}
+
+func (self *_VidMap) define(vid string, reg Reg) {
+    self.m[vid] = reg
+}
+
 // CSE performs the Common Sub-expression Elimintation optimization.
 type CSE struct{}
 
-func (CSE) Apply(cfg *CFG) {
-    for {
-        done := true
-        vals := make(map[string]Reg)
+func (self CSE) dfs(cfg *CFG, bb *BasicBlock, vm *_VidMap) {
+    ins := bb.Ins
+    vals := vm.derive()
 
-        /* replace all the values with same VID with a copy to the first occurance */
-        cfg.ReversePostOrder(func(bb *BasicBlock) {
-            for i, v := range bb.Ins {
-                var r Reg
-                var d _VID
-                var ok bool
+    /* scan every instructions */
+    for i, v := range ins {
+        var r Reg
+        var d _Vid
+        var ok bool
 
-                /* check if the instruction have VIDs */
-                if d, ok = v.(_VID); !ok {
-                    continue
-                }
+        /* check if the instruction have VIDs */
+        if d, ok = v.(_Vid); !ok {
+            continue
+        }
 
-                /* calculate the VIDs */
-                repc := i
-                vids := d.vid()
-                defs := d.Definations()
+        /* calculate the VIDs */
+        repc := i
+        vids := d.vid()
+        defs := d.Definations()
 
-                /* replace each VID with a copy instruction */
-                for j, vid := range vids {
-                    s := defs[j]
-                    r, ok = vals[vid]
+        /* replace each VID with a copy instruction */
+        for j, vid := range vids {
+            s := defs[j]
+            r, ok = vals.lookup(vid)
 
-                    /* skip zero registers */
-                    if s.kind() == _K_zero {
-                        continue
-                    }
-
-                    /* add to definations if not found */
-                    if r, ok = vals[vid]; !ok {
-                        vals[vid] = *s
-                        continue
-                    }
-
-                    /* allocate one slot for the new instruction */
-                    repc++
-                    bb.Ins = append(bb.Ins, nil)
-                    copy(bb.Ins[repc + 1:], bb.Ins[repc:])
-
-                    /* insert a new copy instruction */
-                    t := *s
-                    *s, done = s.zero(), false
-                    bb.Ins[repc] = IrCopy(t, r)
-                }
-
-                /* all the definations are been replaced */
-                if repc == i + len(defs) {
-                    bb.Ins = append(bb.Ins[:i], bb.Ins[i + 1:]...)
-                }
+            /* skip zero registers */
+            if s.kind() == _K_zero {
+                continue
             }
-        })
 
-        /* no modifications in this round */
-        if done {
-            break
+            /* add to definations if not found */
+            if !ok {
+                vals.define(vid, *s)
+                continue
+            }
+
+            /* allocate one slot for the new instruction */
+            repc++
+            bb.Ins = append(bb.Ins, nil)
+            copy(bb.Ins[repc + 1:], bb.Ins[repc:])
+
+            /* insert a new copy instruction */
+            bb.Ins[repc] = IrCopy(*s, r)
+            *s = s.zero()
+        }
+
+        /* all the definations are been replaced */
+        if repc == i + len(defs) {
+            bb.Ins = append(bb.Ins[:i], bb.Ins[i + 1:]...)
         }
     }
+
+    /* DFS the dominator tree */
+    for _, v := range cfg.DominatorOf[bb.Id] {
+        self.dfs(cfg, v, vals)
+    }
+}
+
+func (self CSE) Apply(cfg *CFG) {
+    self.dfs(cfg, cfg.Root, nil)
 }
